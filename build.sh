a026b4f3b018ee42af65c11e251af42cdd692b06
topjohnwu <topjohnwu@gmail.com>
2022-04-06 05:40:26 -0700

    Cross compile aarch64 for macOS

--- build.sh
+++ build.sh
@@ -2,23 +2,36 @@
 
 set -e
 
+if [ -z $1 ]; then
+  echo "Usage: $0 <arch>"
+  echo "<arch> is either x86_64 or aarch64"
+  echo
+  exit 1
+fi
+
 RUST_VERSION='beta'
 NDK_VERSION='r24'
 OUTPUT_VERSION='r24.0'
 OS=$(uname | tr '[:upper:]' '[:lower:]')
+ARCH="$1"
 
 if [ $OS = "darwin" ]; then
   NDK_DIRNAME='darwin-x86_64'
-  TRIPLE='x86_64-apple-darwin'
+  TRIPLE="${ARCH}-apple-darwin"
   DYN_EXT='dylib'
 
-  command -v ninja || brew install ninja
+  if [ $ARCH = "aarch64" ]; then
+    # Configure jemalloc to use 64k pages for Apple Silicon
+    export JEMALLOC_SYS_WITH_LG_PAGE=16
+  fi
+
+  command -v ninja >/dev/null || brew install ninja
 else
   NDK_DIRNAME='linux-x86_64'
-  TRIPLE='x86_64-unknown-linux-gnu'
+  TRIPLE="${ARCH}-unknown-linux-gnu"
   DYN_EXT='so'
 
-  command -v ninja || sudo apt-get install ninja-build
+  command -v ninja >/dev/null || sudo apt-get install ninja-build
 fi
 
 clone() {
@@ -41,7 +54,7 @@ clone() {
 
 build() {
   cd rust
-  python3 ./x.py --config "../config-${OS}.toml" install
+  python3 ./x.py --config "../config-${OS}.toml" --host $TRIPLE install
   cd ../
 
   cd out
@@ -92,13 +105,59 @@ ndk() {
   cd ../../../../
 }
 
-archive() {
+dist() {
   mv ndk "ondk-${OUTPUT_VERSION}"
   mkdir dist
   tar zcvf "dist/ondk-${OUTPUT_VERSION}-${OS}.tar.gz" "ondk-${OUTPUT_VERSION}"
 }
 
+universal() {
+  cp -af out.x86 out
+  cp -an out.arm/. out/. || true
+
+  # Replace lld link with universal binary
+  rm out/llvm-bin/lld
+  lipo -create -output out/llvm-bin/lld \
+    out/lib/rustlib/x86_64-apple-darwin/bin/rust-lld \
+    out/lib/rustlib/aarch64-apple-darwin/bin/rust-lld
+
+  # Merge all Mach-O files as universal binary and adhoc codesign
+  find out -type f -exec sh -c "file {} | grep -q Mach-O" \; -print0 | \
+  while IFS= read -r -d '' o; do
+    local a="${o/out/out.x86}"
+    local b="${o/out/out.arm}"
+    if [ -f "$a" -a -f "$b" ]; then
+      lipo -create -output "$o" "$a" "$b"
+    fi
+    codesign -s - "$o"
+  done
+}
+
 clone
+
+if [ $OS = "darwin" -a $ARCH = "aarch64" ]; then
+  if [ ! -f tmp/stage-1.tar.gz ]; then
+    echo '! Missing stage 1 artifacts'
+    exit 1
+  fi
+  tar zxvf tmp/stage-1.tar.gz
+  mv out out.x86
+fi
+
 build
+
+if [ $OS = "darwin" ]; then
+  if [ $ARCH = "x86_64" ]; then
+    # Pack up first stage artifacts
+    mkdir tmp
+    tar zcvf tmp/stage-1.tar.gz rust/build/$TRIPLE out
+    # Exit early
+    exit 0
+  else
+    mv out out.arm
+    universal
+  fi
+fi
+
 ndk
-archive
+dist
