1a3999032e2506890bde4f0e28f6e034918bab7d
topjohnwu <topjohnwu@gmail.com>
2023-06-05 02:24:29 -0700

    Update Windows scripts

--- build-windows.sh
+++ build-windows.sh
@@ -20,39 +20,6 @@ NDK_DIRNAME='windows-x86_64'
 TRIPLE='x86_64-pc-windows-gnu'
 DYN_EXT='dll'
 
-link() {
-  local BASE="$(realpath -s "$PWD")"
-  local TARGET="$(realpath -s "$2")"
-  if [ ! -d "$TARGET" ]; then
-    local NAME="$(basename "$TARGET")"
-    local TARGET="$(dirname "$TARGET")"
-  else
-    local NAME="$(basename "$1")"
-  fi
-  local SRC="$(realpath -s --relative-to="$TARGET" "$1")"
-  cd "$TARGET"
-  ln -sf "$SRC" "$NAME"
-  cd "$BASE"
-}
-
-copy() {
-  for a in $1/*; do
-    local b="$2/$(basename $a)"
-    if [ -f $a ] && [ ! -L $a ]; then
-      cp -af $a $b
-    elif [ -L $a ]; then
-      local c="$(realpath $a)"
-      local d="$2/$(basename $c)"
-      cp -af $c $d
-      link $d $b
-    elif [ -d $a ]; then
-      mkdir -p $b
-      copy $a $b &
-    fi
-  done
-  wait
-}
-
 clean_storage() {
   # Clean up storage to fit in all our build output
   rm -rf /c/SeleniumWebDrivers /c/selenium /c/Android /c/tools /c/Modules \
@@ -63,18 +30,17 @@ clean_storage() {
 
 build() {
   cd rust
-  python ./x.py \
-    --config '../config-windows.toml' \
-    --build $TRIPLE install
+  python ./x.py --config '../config-windows.toml' --host $TRIPLE install
   cd ../
 
   cd out
-  mkdir -p llvm-bin
-  copy ../rust/build/$TRIPLE/llvm/bin llvm-bin
-  copy ../rust/build/$TRIPLE/lld/bin llvm-bin
-
-  link llvm-bin/lld.exe llvm-bin/ld.exe
+  find . -name '*.old' -delete
+  cp -af ../rust/build/$TRIPLE/llvm/bin llvm-bin || true
+  cp -an ../rust/build/$TRIPLE/llvm/bin/. llvm-bin/.
   cp -af $(../rust/build/$TRIPLE/llvm/bin/clang -print-resource-dir)/include clang-include
+  cp -af lib/rustlib/$TRIPLE/bin/rust-lld.exe llvm-bin/lld.exe
+  ln -sf lld.exe llvm-bin/ld.exe
+  ln -sf lld.exe llvm-bin/ld.lld.exe
   cd ..
 }
 
@@ -84,16 +50,14 @@ ndk() {
   # Download and extract
   [ -f $NDK_ZIP ] || curl -O -L "https://dl.google.com/android/repository/$NDK_ZIP"
   unzip -q $NDK_ZIP
-  rm -rf ndk
   mv "android-ndk-${NDK_VERSION}" ndk
 
-  curl -o mingw.7z -O -L "https://github.com/niXman/mingw-builds-binaries/releases/download/12.2.0-rt_v10-rev2/x86_64-12.2.0-release-win32-seh-ucrt-rt_v10-rev2.7z"
-  7z x mingw.7z
-
   # Copy the whole output folder into ndk
-  copy out ndk/toolchains/rust
+  cp -af out ndk/toolchains/rust || true
+  cp -an out/. ndk/toolchains/rust/.
 
   cd ndk/toolchains
+
   local LLVM_DIR=llvm/prebuilt/$NDK_DIRNAME
 
   # Replace headers
@@ -101,48 +65,31 @@ ndk() {
   rm -rf $NDK_RES/include
   mv rust/clang-include $NDK_RES/include
 
-  # Replace llvm files with those from the rust toolchain
-  for b in $LLVM_DIR/bin/*; do
-    local a="rust/llvm-bin/$(basename $b)"
-    if [ -f $a ] && [ ! -L $a ]; then
-      cp -af $a $b
-    elif [ -L $a ]; then
-      local c="$(realpath $a)"
-      local d=$LLVM_DIR/bin/$(basename $c)
-      cp -af $c $d
-      link $d $b
-    fi
-  done
-
-  # Copy extra useful tools
-  for a in rust/llvm-bin/*; do
-    local b="$LLVM_DIR/bin/$(basename $a)"
-    if [[ $(basename $a) == clang* ]] && [ ! -f $b ] && [ ! -L $b ]; then
-        if [ ! -L $a ]; then
-          cp -af $a $b
-        elif [ -L $a ]; then
-          local c="$(realpath $a)"
-          local d=$LLVM_DIR/bin/$(basename $c)
-          cp -af $c $d
-          link $d $b
-        fi
-    fi
-  done
-
-  # Move library
-  mkdir -p $(dirname $($LLVM_DIR/bin/clang -print-resource-dir))
-  mv $NDK_RES $($LLVM_DIR/bin/clang -print-resource-dir)
-
+  # Replace files with those from the rust toolchain
+  cp -af rust/llvm-bin/. $LLVM_DIR/bin/. || true
+  cp -an rust/llvm-bin/. $LLVM_DIR/bin/.
   rm -rf rust/llvm-bin
-  cp -af ../../mingw64/* rust/
 
-  cd ../../
+  # Now that clang is replaced, move files to the correct location
+  local NEW_NDK_RES=$($LLVM_DIR/bin/clang -print-resource-dir)
+  mkdir -p $(dirname $NEW_NDK_RES)
+  mv $NDK_RES $NEW_NDK_RES
+  cd ../..
 }
 
 export PATH='/c/Program Files/Git/cmd':$PATH
 
-clean_storage
+if [ -n "$GITHUB_ACTION" ]; then
+  clean_storage
+fi
+
 clone
 build
 ndk
+
+# Bundle the entire mingw toolchain
+curl -o mingw.7z -O -L "https://github.com/niXman/mingw-builds-binaries/releases/download/13.1.0-rt_v11-rev1/x86_64-13.1.0-release-win32-seh-ucrt-rt_v11-rev1.7z"
+7z x mingw.7z
+cp -af mingw64/. ndk/toolchains/rust/.
+
 dist
